11cd94e36b99d2441f66b344e2879d9dac937b53
chatchaijc <41831881+chatchaijc@users.noreply.github.com>
2021-03-03 21:42:28 +0700

    create learn part2
    create learn part2 lab1.5

---
+++ src/app/lab15/lab15.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lab15',
+  templateUrl: './lab15.component.html',
+  styleUrls: ['./lab15.component.css']
+})
+export class Lab15Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
